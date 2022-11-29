73761b8ea4da7a212efe331a49682789f485c79c
quangtranvan1501 <81863404+quangtranvan1501@users.noreply.github.com>
2022-11-29 11:36:00 +0700

    update chỉnh sửa tên hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 10, name: 'Súp pơ A'}, 
+    {id: 11, name: 'Súp pơ AB'}, 
+    {id: 12, name: 'Súp pơ ABC'}, 
+    {id: 13, name: 'Súp pơ ABCD'}, 
+    {id: 14, name: 'Súp pơ ABCDE'}, 
+    {id: 15, name: 'Súp pơ ABCDEF'}, 
+    {id: 16, name: 'Súp pơ ABCDEFG'}, 
+    {id: 17, name: 'Súp pơ ABCDEFGH'}, 
+    {id: 18, name: 'Súp pơ ABCDEFGHI'}, 
+
+]
